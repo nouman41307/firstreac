94f365e6b027bab9edd09913901829aa70175672
Muhammad Nauman <nouman41307@github.com>
2020-11-21 15:02:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import Cars from './cars'
 function App() {
- return(
-
+    let [count , setCount]= useState(1);
+    return(
+<div>
 <Cars brandH="Honda" brandT="Toyota" brandS="Suzuki"/>
-
+<h5>The counter is :{count}</h5>
+<br/>
+<button onClick ={()=>setCount(count + 1)}> Update Counter </button>
+</div>
  );
 }
 
